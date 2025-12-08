2fbb1a0cd31091da97afea7fba46855299de7fc8
Gabriel Daiki <daiki.gab@gmail.com>
2025-12-08 19:13:22 -0300

    feat: api gateway

---
+++ gateway/src/main/java/com/clownstore/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.clownstore.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests(
+                        c-> c.anyRequest().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(c-> c.jwt(Customizer.withDefaults()))
+                .csrf(c->c.disable());
+        return http.build();
+    }
+}
