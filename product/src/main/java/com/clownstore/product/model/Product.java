8edc03bad63395cc12c0645e8cd68bb8accc3229
Gabriel Daiki <daiki.gab@gmail.com>
2025-12-15 15:08:37 -0300

    test: unit tests for product

--- product/src/main/java/com/clownstore/product/model/Product.java
+++ product/src/main/java/com/clownstore/product/model/Product.java
@@ -4,6 +4,7 @@ import com.mongodb.annotations.Beta;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
--- product/src/test/java/com/clownstore/product/ProductApplicationTests.java
+++ product/src/test/java/com/clownstore/product/ProductApplicationTests.java
@@ -1,13 +1,57 @@
 package com.clownstore.product;
 
+import com.clownstore.product.exception.ProductExistsException;
+import com.clownstore.product.model.Product;
+import com.clownstore.product.repository.ProductRepository;
+import com.clownstore.product.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class ProductApplicationTests {
 
+	@Mock
+	ProductRepository repository;
+
+	@InjectMocks
+	ProductService service;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("Test if duplicated products can save")
+	void test1() {
+		Product mock = new Product("abc","Coffe", BigDecimal.TEN, 10, "dark coffe", Map.of("origin", "arabic"));
+		when(repository.findByNameContaining(mock.getName())).thenReturn(Optional.of(mock));
+		assertThrows(ProductExistsException.class, ()-> service.save(mock));
+
+		verify(repository, never()).save(any(Product.class));
 	}
 
+	@Test
+	@DisplayName("Test if a saved entity updates price by name")
+	void test2(){
+		Product mock = new Product("abc","Coffe", BigDecimal.TEN, 10, "dark coffe", Map.of("origin", "arabic"));
+		when(repository.findByNameContaining(mock.getName())).thenReturn(Optional.of(mock));
+
+		service.updatePriceByName(BigDecimal.ONE, mock.getName());
+
+		verify(repository).save(mock);
+
+		assertEquals(BigDecimal.ONE, mock.getPrice());
+	}
 }
